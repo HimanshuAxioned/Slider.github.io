6b1e0dd7591dcc19a2ff5146b722bad6ce0dd471
himanshu <himanshup@axioned.com>
2021-09-24 11:52:25 +0530

    added last slide in js

--- main.js
+++ main.js
@@ -19,15 +19,20 @@ slides.forEach(function (s) {
 })
 
 arrow.forEach(function (a) {
-  a.addEventListener('click', function () {
+  a.addEventListener('click', function (e) {
     let active = document.querySelector('.active');
     let activeSlide = slide.indexOf(active)
     let nextSlide;
     let previousSlide;
 
+
+
+
+
     // next slide
     if (activeSlide == slide.length - 1) {
-      nextSlide = slide[0];
+      nextSlide = slide[0]
+
     } else {
       nextSlide = slide[activeSlide + 1]
     }
@@ -35,6 +40,7 @@ arrow.forEach(function (a) {
     // previous slide
     if (activeSlide == 0) {
       previousSlide = slide[slide.length - 1];
+
     } else {
       previousSlide = slide[activeSlide - 1];
     }
@@ -43,35 +49,51 @@ arrow.forEach(function (a) {
     const leftArrow = document.querySelector('.left-arrow');
     const rightArrow = document.querySelector('.right-arrow');
 
-    slider.style.overflowX = 'hidden';
+
+    let oppositeSilde = Math.abs(activeSlide - 2);
+    slide[oppositeSilde].classList.add('hide')
+
+    let hide = document.querySelector('.hide');
+    hide.style.visibility = 'hidden';
+
+    let lastSlide = slide[slide.length - 1]
 
     if (a == leftArrow) {
+      lastSlide.style.visibility = 'hidden';
       leftSide();
+      hide.classList.remove('hide');
     }
     else if (a == rightArrow) {
+      lastSlide.style.visibility = 'hidden';
       rightSide();
+      hide.classList.remove('hide');
     }
 
+    
+
+
     function leftSide() {
-      active.style.transition = '0.2s';
-      active.style.transform = 'translateX(0)';
-      active.classList.remove('active');
+
       nextSlide.classList.add('active');
+      active.style.transform = 'translateX(0)';
       previousSlide.style.transform = 'translateX(-150%)';
       nextSlide.style.transform = 'translateX(150%)';
-      nextSlide.style.transition = 'none';
+      active.style.transition = '1s';
+      active.style.visibility = 'visible';
+      active.classList.remove('active');
+     
     }
 
     function rightSide() {
-      active.style.transition = '0.2s';
-      active.style.transform = 'translateX(0)';
-      active.classList.remove('active');
+      active.style.transition = '1s';
       previousSlide.classList.add('active');
+      active.classList.remove('active');
+      active.style.transform = 'translateX(0)';
+      active.style.visibility = 'visible';
       nextSlide.style.transform = 'translateX(150%)';
       previousSlide.style.transform = 'translateX(-150%)';
-      previousSlide.style.transition = 'none';
-      
     }
+
   })
 })
 
