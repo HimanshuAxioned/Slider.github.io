417f58baa8206ab1c7d672818f632448fbc9e566
himanshu <himanshup@axioned.com>
2021-09-25 11:21:13 +0530

    final code in javascript

--- main.js
+++ main.js
@@ -1,188 +1,126 @@
-// const hamburger = document.querySelector('.hamburger');
-// const navList = document.querySelectorAll('.nav-list');
-// const slider = document.querySelector('.banner-content');
-// const slides = document.querySelectorAll('.slide');
-// const arrow = document.querySelectorAll('.banner-arrow');
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelectorAll('.nav-list');
+const slider = document.querySelector('.banner-content');
+const slides = document.querySelectorAll('.slide');
+const arrow = document.querySelectorAll('.banner-arrow');
 
-// // hamburger
-// hamburger.addEventListener('click', function () {
+// hamburger
+hamburger.addEventListener('click', function () {
 
-//   navList.forEach(function (n) {
-//     n.classList.toggle('responsive-nav');
-//   })
-//   hamburger.classList.toggle('close');
-// })
+  navList.forEach(function (n) {
+    n.classList.toggle('responsive-nav');
+  })
+  hamburger.classList.toggle('close');
+})
 
-// const slide = [];
-// slides.forEach(function (s) {
-//   slide.push(s);
-// })
+const slide = [];
+slides.forEach(function (s) {
+  slide.push(s);
+})
 
-// function previousNext() {
-//   let active = document.querySelector('.active');
-//   let activeSlide = slide.indexOf(active);
-//   let nextSlide;
-//   let previousSlide;
+function previousNext() {
+  let active = document.querySelector('.active');
+  let activeSlide = slide.indexOf(active);
+  let nextSlide;
+  let previousSlide;
 
-//   //next slide
-//   if (activeSlide == slide.length - 1) {
-//     nextSlide = slide[0]
+  //next slide
+  if (activeSlide == slide.length - 1) {
+    nextSlide = slide[0]
 
-//   } else {
-//     nextSlide = slide[activeSlide + 1]
-//   }
+  } else {
+    nextSlide = slide[activeSlide + 1]
+  }
 
-//   // previous slide
-//   if (activeSlide == 0) {
-//     previousSlide = slide[slide.length - 1];
+  // previous slide
+  if (activeSlide == 0) {
+    previousSlide = slide[slide.length - 1];
 
-//   } else {
-//     previousSlide = slide[activeSlide - 1];
-//   }
+  } else {
+    previousSlide = slide[activeSlide - 1];
+  }
 
-//   return [nextSlide, previousSlide]
+  return [nextSlide, previousSlide]
 
-// }
+}
 
-// function slidePosition() {
-//   let active = document.querySelector('.active');
-//   let activeSlide = slide.indexOf(active);
-//   let prevNext = previousNext();
-//   let next = prevNext[0];
-//   let previous = prevNext[1]
+function slidePosition() {
+  let active = document.querySelector('.active');
+  let activeSlide = slide.indexOf(active);
+  let prevNext = previousNext();
+  let next = prevNext[0];
+  let previous = prevNext[1]
 
-//   slide.forEach(function (s, i) {
-//     if (i == activeSlide) {
-//       s.style.transform = 'translateX(0)';
-//     } else if (s == previous) {
-//       s.style.transform = 'translateX(-150%)';
-//     } else if (s == next) {
-//       s.style.transform = 'translateX(150%)';
-//     } else {
-//       s.style.transform = 'translateX(150%)';
-//     }
-//   })
-// }
+  slide.forEach(function (s, i) {
+    if (i == activeSlide) {
+      s.style.transform = 'translateX(0)';
+    } else if (s == previous) {
+      s.style.transform = 'translateX(-150%)';
+    } else if (s == next) {
+      s.style.transform = 'translateX(150%)';
+    } else {
+      s.style.transform = 'translateX(150%)';
+    }
 
-// slidePosition();
-
-// arrow.forEach(function (a) {
-//   a.addEventListener('click', function () {
-//     const leftArrow = document.querySelector('.left-arrow');
-//     const rightArrow = document.querySelector('.right-arrow');
-
-//     if (a == leftArrow) {
-//       leftSide();
-//     }
-//     else if (a == rightArrow) {
-//       rightSide();
-//     }
-//   })
-// })
-
-// function leftSide() {
-
-//   let active = document.querySelector('.active');
-//   let prevNext = previousNext();
-//   let next = prevNext[0];
-
-//   next.style.zIndex= '3';
-//   active.classList.remove('active');
-//   active.style.transition = '1s'
-//   active.style.transform = 'translateX(-150%)';
-//   next.style.transform = 'translateX(0)';
-//   next.classList.add('active')
-//   next.style.transition = '1s'
-//   slidePosition();
-// }
-
-// function rightSide() {
-//   let active = document.querySelector('.active');
-//   let prevNext = previousNext();
-//   let previous = prevNext[1];
-
-//   active.classList.remove('active');
-//   active.style.transition = '1s'
-//   active.style.transform = 'translateX(150%)';
-//   previous.style.transform = 'translateX(0)';
-//   previous.classList.add('active')
-//   previous.style.transition = '1s'
-//   slidePosition();
-// }
-
-// arrow.forEach(function (a) {
-//   a.addEventListener('click', function () {
-//     let active = document.querySelector('.active');
-//     let activeSlide = slide.indexOf(active);
-//     let nextSlide;
-//     let previousSlide;
-//     // next slide
-//     if (activeSlide == slide.length - 1) {
-//       nextSlide = slide[0]
-
-//     } else {
-//       nextSlide = slide[activeSlide + 1]
-//     }
-
-//     // previous slide
-//     if (activeSlide == 0) {
-//       previousSlide = slide[slide.length - 1];
-
-//     } else {
-//       previousSlide = slide[activeSlide - 1];
-//     }
     
 
+    s.addEventListener('transitionend', function(){
+      s.classList.remove('top')
+    })
+    if (s.classList.contains('top')){
+      s.style.transition = '1s';
+    } else {
+      s.style.transition = '0s';
+    }
+  })
+}
 
-//     const leftArrow = document.querySelector('.left-arrow');
-//     const rightArrow = document.querySelector('.right-arrow');
+slidePosition();
 
-//     // slider.style.overflowX = 'hidden';
+arrow.forEach(function (a) {
+  a.addEventListener('click', function () {
+    const leftArrow = document.querySelector('.left-arrow');
+    const rightArrow = document.querySelector('.right-arrow');
 
-//     let oppositeSilde = Math.abs(activeSlide - 2);
-//     slide[oppositeSilde].classList.add('hide')
+    if (a == leftArrow) {
+      leftSide();
+    }
+    else if (a == rightArrow) {
+      rightSide();
+    }
+  })
+})
 
-//     let hide = document.querySelector('.hide');
-//     hide.style.visibility = 'hidden';
+function leftSide() {
 
-//     let lastSlide = slide[slide.length - 1]
+  let active = document.querySelector('.active');
+  let prevNext = previousNext();
+  let next = prevNext[0];
 
-   
+  active.classList.add('top');
+  next.classList.add('top');
+  active.classList.remove('active');
+  active.style.transition = '1s'
+  active.style.transform = 'translateX(-150%)';
+  next.style.transform = 'translateX(0)';
+  next.classList.add('active')
+  next.style.transition = '1s'
+  slidePosition();
+}
 
-//     if (a == leftArrow) {
-//       lastSlide.style.visibility = 'hidden';
-//       leftSide();
-//       hide.classList.remove('hide');
-//     }
-//     else if (a == rightArrow) {
-//       lastSlide.style.visibility = 'hidden';
-//       rightSide();
-//       hide.classList.remove('hide');
-//     }
+function rightSide() {
+  let active = document.querySelector('.active');
+  let prevNext = previousNext();
+  let previous = prevNext[1];
 
-//     function leftSide() {
-
-//       nextSlide.classList.add('active');
-//       active.style.transform = 'translateX(0)';
-//       previousSlide.style.transform = 'translateX(-150%)';
-//       nextSlide.style.transform = 'translateX(150%)';
-//       active.style.transition = '1s';
-//       active.style.visibility = 'visible';
-//       active.classList.remove('active');
-
-//     }
-
-//     function rightSide() {
-//       active.style.transform = 'translateX(150%)';
-//       active.style.transition = '1s';
-//       previousSlide.classList.add('active');
-//       active.classList.remove('active');
-//       active.style.transform = 'translateX(0)';
-//       active.style.visibility = 'visible';
-//       nextSlide.style.transform = 'translateX(150%)';
-//       previousSlide.style.transform = 'translateX(-150%)';
-//     }
-
-//   })
-// })
+  active.classList.add('top');
+  previous.classList.add('top');
+  active.classList.remove('active');
+  active.style.transition = '1s'
+  active.style.transform = 'translateX(150%)';
+  previous.style.transform = 'translateX(0)';
+  previous.classList.add('active')
+  previous.style.transition = '1s'
+  slidePosition();
+}
 
